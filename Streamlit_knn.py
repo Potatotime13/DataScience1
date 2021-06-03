5df7ca963cfe2f54581c9a70bee9a3645890d233
Lucas <lucas.j.menger@gmail.com>
2021-06-03 15:54:00 +0200

    les go

---
+++ Streamlit_knn.py
@@ -0,0 +1,59 @@
+import streamlit as st
+# To make things easier later, we're also importing numpy and pandas for
+# working with sample data.
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from pathlib import Path
+import re
+
+
+def main():
+    st.title('KNN')
+    with st.sidebar:
+        st.write('Dataset selection')
+
+    task1()
+
+    c_task = st.sidebar.selectbox(
+        "",
+        ("MovieLens", "Books")
+    )
+    if c_task == "1.1 Clustering":
+        task1()
+
+
+def task1():
+    # read movie lens
+    movies = pd.read_csv('movies.csv')
+    ratings = pd.read_csv('ratings.csv')
+    tags = pd.read_csv('tags.csv')
+    st.write('Task 1')
+    with st.beta_expander("display code"):
+        with st.echo('below'):
+            # rating table
+            df_rating = ratings.pivot(index="movieId", columns="userId", values="rating")
+            df_rating_raw = df_rating
+
+            # centering
+            df_rating = df_rating - df_rating.mean()
+            df_rating = df_rating.fillna(0)
+            user_std = (df_rating * df_rating).mean() ** 0.5
+            # calc cov matrix
+            user_corr = df_rating.cov() / (user_std.values.reshape((-1, 1)) @ user_std.values.reshape((1, -1)))
+            user_corr = user_corr.fillna(0)
+
+            user_number = 10
+            k_users = 15
+            sorted_index = list(np.argsort(user_corr[user_number]))[::-1]
+            recommended_amount_of_dedotated_wam = np.zeros(len(df_rating))
+            for k in range(1, k_users+1):
+                mov = user_corr[user_number][sorted_index[k]] * df_rating[sorted_index[k]].values
+                recommended_amount_of_dedotated_wam += mov
+            recommended_amount_of_dedotated_wam *= df_rating_raw[user_number].isnull().values
+            sorted_mov = list(np.argsort(recommended_amount_of_dedotated_wam))[::-1]
+            print(sorted_mov[0:10])
+
+
+if __name__ == "__main__":
+    main()
--- firsttests.py
+++ firsttests.py
@@ -1,17 +1,15 @@
 import pandas
-
+from IPython.display import display
 # read movie lens
 movies = pandas.read_csv('movies.csv')
 ratings = pandas.read_csv('ratings.csv')
 tags = pandas.read_csv('tags.csv')
-rating_table = pandas.read_csv('rating_table.csv')
 
 # split the genres per movie
-m_info_map = []
+movies["genres"] = movies["genres"].str.split('|')
 genres = []
 for index, movie in movies.iterrows():
-    m_info_map.append([movie['genres'].split('|'), []])
-    for g in movie['genres'].split('|'):
+    for g in movie['genres']:
         if g not in genres:
             genres.append(g)
 
@@ -29,27 +27,19 @@ movie_map = movies['movieId'].values.tolist()
 for index, row in tags.iterrows():
     i = movie_map.index(row['movieId'])
     r = row['tag']
-    m_info_map[i][1].append(r)
-
-for ind in range(len(m_info_map)):
-    for ind2 in range(len(m_info_map[ind][0])):
-        m_info_map[ind][0][ind2] = gen_dict[m_info_map[ind][0][ind2]]
-    m_info_map[ind].insert(1, len(m_info_map[ind][1]))
 
 # rating table
-if False:
-    user_map = [ratings['userId'][0]]
-    rating_table = [[0.0 for i in range(len(movie_map))]]
-    for index, rating in ratings.iterrows():
-        if user_map[-1] == rating['userId']:
-            rating_table[-1][movie_map.index(rating['movieId'])] = rating['rating']
-        else:
-            rating_table.append([0 for i in range(len(movie_map))])
-            rating_table[-1][movie_map.index(rating['movieId'])] = rating['rating']
-            user_map.append(rating['userId'])
-    df2 = pandas.DataFrame(rating_table)
-    df2.to_csv('rating_table.csv')
+df_rating = ratings.pivot(index="movieId", columns="userId", values="rating")
 
-df = pandas.DataFrame(m_info_map)
+# centering
+df_rating = df_rating-df_rating.mean()
+df_rating = df_rating.fillna(0)
+user_std = (df_rating * df_rating).mean()**0.5
+# calc cov matrix
+user_corr = df_rating.cov() / (user_std.values.reshape((-1, 1)) @ user_std.values.reshape((1, -1)))
+user_corr = user_corr.fillna(0)
+
+display(user_corr[53])
+
 
 
