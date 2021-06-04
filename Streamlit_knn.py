1f479a7fee33fba34562b922d70845ebdcd14477
Lucas <lucas.j.menger@gmail.com>
2021-06-04 12:16:54 +0200

    layout

--- Streamlit_knn.py
+++ Streamlit_knn.py
@@ -11,14 +11,14 @@ def main():
     with st.sidebar:
         st.write('Dataset selection')
 
-    task1()
+    #task1()
 
     c_task = st.sidebar.selectbox(
         "",
         ("MovieLens", "Books")
     )
-    #if c_task == "MovieLens":
-    #    task1()
+    if c_task == "MovieLens":
+        task1()
 
 
 def task1():
@@ -68,9 +68,8 @@ def task1():
         use_container_width=True,
         showlegend=False,
     )
-    fig.show()
-    #table_style = {'width': '100%', 'border': '1px solid black'}
-    #st.table(output, style=table_style)
+    #fig.show()
+    st.table(output)
 
 
 if __name__ == "__main__":
