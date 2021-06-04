c88027643f92ade8def33936c02f9fc05d5ef1a5
Lucas <lucas.j.menger@gmail.com>
2021-06-04 16:16:50 +0200

    layout

--- Streamlit_knn.py
+++ Streamlit_knn.py
@@ -13,14 +13,16 @@ def main():
     with st.sidebar:
         st.write('Dataset selection')
 
-    #task1()
+    task1()
 
     c_task = st.sidebar.selectbox(
         "",
-        ("MovieLens", "Books")
+        ("MovieLens", "MovieLens-Tech", "Books")
     )
     if c_task == "MovieLens":
         task1()
+    if c_task == "MovieLens-Tech":
+        task2()
 
 
 def movie_url(ids):
@@ -50,6 +52,66 @@ def task1():
     k_users = st.sidebar.selectbox("K nearest", (15, 20))
     list_len = st.sidebar.selectbox("Recommendations", (10, 40))
 
+    # rating table
+    df_rating = ratings.pivot(index="movieId", columns="userId", values="rating")
+    df_rating_raw = df_rating
+
+    # centering
+    df_rating = df_rating - df_rating.mean()
+    df_rating = df_rating.fillna(0)
+    user_std = (df_rating * df_rating).mean() ** 0.5
+    # calc cov matrix
+    user_corr = df_rating.cov() / (user_std.values.reshape((-1, 1)) @ user_std.values.reshape((1, -1)))
+    user_corr = user_corr.fillna(0)
+
+    sorted_index = list(np.argsort(user_corr[user_number]))[::-1]
+    recommended_amount_of_dedotated_wam = np.zeros(len(df_rating))
+    for k in range(1, k_users+1):
+        mov = user_corr[user_number][sorted_index[k]] * df_rating[sorted_index[k]].values
+        recommended_amount_of_dedotated_wam += mov
+    recommended_amount_of_dedotated_wam *= df_rating_raw[user_number].isnull().values
+    sorted_mov = list(np.argsort(recommended_amount_of_dedotated_wam))[::-1]
+    output = movies.iloc[sorted_mov[0:list_len]][['title', 'genres']]
+
+    fig = go.Figure(data=[go.Table(
+        header=dict(values=list(output.columns)),
+        cells=dict(values=[output.title, output.genres]))
+    ])
+    fig.update_layout(
+        autosize=True,
+        showlegend=False,
+    )
+
+    fig.show()
+
+    url, info = movie_url(links.iloc[sorted_mov[0:3]][['tmdbId']].values)
+
+    st.write('Deine Top auswahl')
+
+    col1, col2, col3 = st.beta_columns(3)
+    col4, col5, col6 = st.beta_columns(3)
+
+    col1.header(info[0])
+    col4.image('https://www.themoviedb.org/t/p/w600_and_h900_bestv2/' + url[0])
+    col2.header(info[1])
+    col5.image('https://www.themoviedb.org/t/p/w600_and_h900_bestv2/' + url[1])
+    col3.header(info[2])
+    col6.image('https://www.themoviedb.org/t/p/w600_and_h900_bestv2/' + url[2])
+
+    st.write('Alle Empfehlungen für dich:')
+    st.table(output)
+
+
+def task2():
+    # read movie lens
+    movies = pd.read_csv('movies.csv')
+    ratings = pd.read_csv('ratings.csv')
+    tags = pd.read_csv('tags.csv')
+    links = pd.read_csv('links.csv')
+    st.write('K nearest neighbor centered cosine distance')
+    user_number = 10
+    k_users = 10
+    list_len = 10
     with st.beta_expander("display code"):
         with st.echo('below'):
             # rating table
@@ -72,34 +134,6 @@ def task1():
             recommended_amount_of_dedotated_wam *= df_rating_raw[user_number].isnull().values
             sorted_mov = list(np.argsort(recommended_amount_of_dedotated_wam))[::-1]
             output = movies.iloc[sorted_mov[0:list_len]][['title', 'genres']]
-
-    fig = go.Figure(data=[go.Table(
-        header=dict(values=list(output.columns)),
-        cells=dict(values=[output.title, output.genres]))
-    ])
-    fig.update_layout(
-        autosize=True,
-        showlegend=False,
-    )
-
-    # fig.show()
-
-    url, info = movie_url(links.iloc[sorted_mov[0:3]][['tmdbId']].values)
-
-    st.write('Deine Top auswahl')
-
-    col1, col2, col3 = st.beta_columns(3)
-    col4, col5, col6 = st.beta_columns(3)
-
-    col1.header(info[0])
-    col4.image('https://www.themoviedb.org/t/p/w600_and_h900_bestv2/' + url[0])
-    col2.header(info[1])
-    col5.image('https://www.themoviedb.org/t/p/w600_and_h900_bestv2/' + url[1])
-    col3.header(info[2])
-    col6.image('https://www.themoviedb.org/t/p/w600_and_h900_bestv2/' + url[2])
-
-    st.write('Alle Empfehlungen für dich:')
-    st.table(output)
 
 
 if __name__ == "__main__":
