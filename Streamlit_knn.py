9d25b8724e1212e90f50be9910dcab0fbbb91cff
Lucas <lucas.j.menger@gmail.com>
2021-06-05 11:27:28 +0200

    color tests

--- Streamlit_knn.py
+++ Streamlit_knn.py
@@ -101,7 +101,7 @@ def task1():
         cells=dict(values=[out2, output.title, output.genres],
                    line_color=['rgb(49, 51, 63)', 'rgb(49, 51, 63)', 'rgb(49, 51, 63)'],
                    fill_color=[np.array(colors), 'rgb(14, 17, 23)', 'rgb(14, 17, 23)'],
-                   align='center', font=dict(color='white', size=14)
+                   align='center', font=dict(color='white', size=14), height=30
                    ))
     ], layout=layout)
 
