cb74363163ba0fd7652e149940237b5b64b836ac
Lucas <lucas.j.menger@gmail.com>
2021-06-14 12:19:03 +0200

    updated network

--- NCF_MovieLens.py
+++ NCF_MovieLens.py
@@ -32,35 +32,40 @@ class ResnetIdentityBlock(tf.keras.Model):
 
 
 class NeuMF(tf.keras.Model):
-    def __init__(self, kernel_size, filters):
+    def __init__(self, dims):
         super(NeuMF, self).__init__(name='')
-        filters1, filters2, filters3 = filters
 
-        self.conv2a = tf.keras.layers.Conv2D(filters1, (1, 1))
-        self.bn2a = tf.keras.layers.BatchNormalization()
+        # input layers
+        self.con_mlp = tf.keras.layers.Concatenate(axis=1)
+        self.mul_gmf = tf.keras.layers.Multiply()
 
-        self.conv2b = tf.keras.layers.Conv2D(filters2, kernel_size, padding='same')
-        self.bn2b = tf.keras.layers.BatchNormalization()
+        # MLP path
+        self.act_mlp = tf.keras.layers.ReLU()
+        self.dens_mlp1 = tf.keras.layers.Dense(dims[0], activation='relu')
+        self.dens_mlp2 = tf.keras.layers.Dense(dims[1], activation='relu')
+        self.dens_mlp3 = tf.keras.layers.Dense(dims[2], activation='relu')
+        self.dens_mlp4 = tf.keras.layers.Dense(dims[3], activation='relu')
 
-        self.conv2c = tf.keras.layers.Conv2D(filters3, (1, 1))
-        self.bn2c = tf.keras.layers.BatchNormalization()
+        # output path
+        self.con_out = tf.keras.layers.Concatenate(axis=1)
+        self.dens_out = tf.keras.layers.Dense(dims[4])
 
-    def call(self, input_tensor, training=False):
-        x = self.conv2a(input_tensor)
-        x = self.bn2a(x, training=training)
-        x = tf.nn.relu(x)
+    def call(self, input_user, input_item, training=False):
+        x1 = self.con_mlp([input_user, input_item])
+        x2 = self.mul_gmf([input_user, input_item])
 
-        x = self.conv2b(x)
-        x = self.bn2b(x, training=training)
-        x = tf.nn.relu(x)
+        x1 = self.act_mlp(x1)
+        x1 = self.dens_mlp1(x1)
+        x1 = self.dens_mlp2(x1)
+        x1 = self.dens_mlp3(x1)
+        x1 = self.dens_mlp4(x1)
 
-        x = self.conv2c(x)
-        x = self.bn2c(x, training=training)
-
-        x += input_tensor
-        return tf.nn.relu(x)
+        x = self.con_out([x1, x2])
+        x = self.dens_out(x)
+        return x
 
 
-block = ResnetIdentityBlock(1, [1, 2, 3])
-_ = block(tf.zeros([1, 2, 3, 3]))
-block.summary()
+neu = NeuMF([10, 20, 30, 30, 1])
+a = neu(tf.zeros([1000000, 10]), tf.ones([1000000, 10]))
+neu.summary()
+print(a)
