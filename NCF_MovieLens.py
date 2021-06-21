fdb9fe3b1dd2f3c34fdd942f3e9c2e4421902102
Lucas <lucas.j.menger@gmail.com>
2021-06-21 09:51:39 +0200

    percentiles update

--- NCF_MovieLens.py
+++ NCF_MovieLens.py
@@ -2,13 +2,15 @@ import tensorflow as tf
 import pandas as pd
 import numpy as np
 from lenskit.algorithms.tf import BiasedMF as bmf
+from lenskit.algorithms.bias import Bias
 
 
 class NeuMF(tf.keras.Model):
-    def __init__(self, dims):
+    def __init__(self, dims, in_min, in_max):
         super(NeuMF, self).__init__(name='')
-
-        self.optimizer = tf.optimizers.Adam(0.02, 0.8, 0.95)
+        self.in_min = in_min
+        self.in_max = in_max
+        self.optimizer = tf.optimizers.Adam()
 
         # input layers
         self.con_mlp = tf.keras.layers.Concatenate(axis=1)
@@ -18,29 +20,36 @@ class NeuMF(tf.keras.Model):
         self.act_mlp = tf.keras.layers.ReLU()
         self.dens_mlp1 = tf.keras.layers.Dense(dims[0], activation=tf.nn.relu)
         self.dens_mlp2 = tf.keras.layers.Dense(dims[1], activation=tf.nn.relu)
-        self.dens_mlp2 = tf.keras.layers.Dense(dims[2], activation=tf.nn.relu)
+        self.dens_mlp3 = tf.keras.layers.Dense(dims[2], activation=tf.nn.relu)
 
         # GMF Path
-        self.dens_gmf1 = tf.keras.layers.Dense(dims[3], activation=tf.nn.tanh)
+        self.dens_gmf1 = tf.keras.layers.Dense(dims[3], activation=tf.nn.relu)
 
         # output path
         self.con_out = tf.keras.layers.Concatenate(axis=1)
-        self.dens_out = tf.keras.layers.Dense(dims[4])
+        self.dens_out = tf.keras.layers.Dense(dims[4], activation=tf.nn.sigmoid)
 
-    def call(self, inputs, training=False):
-        input_user = inputs[0]
-        input_item = inputs[1]
-        x1 = self.con_mlp([input_user, input_item])
-        x2 = self.mul_gmf([input_user, input_item])
+    def call(self, inputs, training=False, mask=None):
+        input_user_g = inputs[0]
+        input_item_g = inputs[1]
+        input_bias_u = inputs[2]
+        input_bias_i = inputs[3]
+        input_user_m = inputs[4]
+        input_item_m = inputs[5]
+
+        x1 = self.con_mlp([input_user_m, input_item_m])
+        x2 = self.mul_gmf([input_user_g, input_item_g])
+        x2 = self.con_out([x2, input_bias_u, input_bias_i])
         x2 = self.dens_gmf1(x2, training=training)
 
         x1 = self.act_mlp(x1)
         x1 = self.dens_mlp1(x1, training=training)
         x1 = self.dens_mlp2(x1, training=training)
+        x1 = self.dens_mlp3(x1, training=training)
 
         x = self.con_out([x1, x2])
         x = self.dens_out(x, training=training)
-        return x
+        return self.in_min + x * (self.in_max - self.in_min)
 
     def train_step(self, data):
         # Unpack the data. Its structure depends on your model and
@@ -64,102 +73,161 @@ class NeuMF(tf.keras.Model):
         return {m.name: m.result() for m in self.metrics}
 
 
-def create_valid(dataset, test_len=5000):
+def get_book_data(filter_tr, like_to_value=True):
+    # load data from csv
+    books = pd.read_csv('BX-Books.csv', sep=';', error_bad_lines=False, encoding="latin-1")
+    books.columns = ['ISBN', 'bookTitle', 'bookAuthor', 'yearOfPublication', 'publisher', 'imageUrlS', 'imageUrlM',
+                     'imageUrlL']
+
+    users = pd.read_csv('BX-Users.csv', sep=';', error_bad_lines=False, encoding="latin-1")
+    users.columns = ["userId", "location", "age"]
+
+    ratings = pd.read_csv('BX-Book-Ratings.csv', sep=';', error_bad_lines=False, encoding="latin-1")
+    ratings.columns = ["userId", "ISBN", "rating"]
+
+    ratings = ratings.drop_duplicates(subset=["userId", "ISBN"])
+
+    # filter dataset for users / items with much interaction
+    u = ratings.userId.value_counts()
+    b = ratings.ISBN.value_counts()
+
+    ratings = ratings[ratings.userId.isin(u.index[u.gt(filter_tr)])]
+    ratings = ratings[ratings.ISBN.isin(b.index[b.gt(filter_tr)])]
+
+    # create table
+    df_ratings = ratings.pivot(index="ISBN", columns="userId", values="rating")
+    df_rating_nonzero = ratings.loc[ratings["rating"].values != 0]
+    if like_to_value:
+        percentiles = df_ratings.describe(include='all').iloc[6].values
+        df_zeros = df_ratings.values == 0
+        df_ratings = df_ratings + df_zeros * percentiles
+        df_zeros = df_ratings == 0
+        df_ratings = df_ratings + df_zeros * np.mean(percentiles[percentiles != 0])
+        ratings["rating"] = df_ratings.stack().values
+
+    return df_ratings, ratings, df_rating_nonzero, books, users
+
+
+def create_valid(dataset, test_len=5000, movie=True):
+    dataset = dataset.reset_index()
     ind_exc = np.random.permutation(len(dataset))[0:test_len]
     test_set = dataset.iloc[ind_exc]
-    dataset.drop(index=ind_exc, inplace=True)
-    use_v = test_set['userId'].unique()
-    mov_v = test_set['movieId'].unique()
-    use_t = dataset['userId'].unique()
-    mov_t = dataset['movieId'].unique()
-    index_u = list(np.intersect1d(use_v, use_t))
-    index_m = list(np.intersect1d(mov_v, mov_t))
-    dataset = dataset.pivot(index="movieId", columns="userId", values="rating")
-    test_set = test_set.pivot(index="movieId", columns="userId", values="rating")
-    dataset = dataset[index_u]
-    test_set = test_set.loc[index_m][index_u]
+    if movie is True:
+        dataset.drop(index=ind_exc, inplace=True)
+        use_v = test_set['userId'].unique()
+        mov_v = test_set['movieId'].unique()
+        use_t = dataset['userId'].unique()
+        mov_t = dataset['movieId'].unique()
+        index_u = list(np.intersect1d(use_v, use_t))
+        index_m = list(np.intersect1d(mov_v, mov_t))
+        dataset = dataset.pivot(index="movieId", columns="userId", values="rating")
+        test_set = test_set.pivot(index="movieId", columns="userId", values="rating")
+        dataset = dataset[index_u]
+        test_set = test_set.loc[index_m][index_u]
+    else:
+        dataset.drop(index=ind_exc, inplace=True)
+        use_v = test_set['userId'].unique()
+        mov_v = test_set['ISBN'].unique()
+        use_t = dataset['userId'].unique()
+        mov_t = dataset['ISBN'].unique()
+        index_u = list(np.intersect1d(use_v, use_t))
+        index_m = list(np.intersect1d(mov_v, mov_t))
+        dataset = dataset.pivot(index="ISBN", columns="userId", values="rating")
+        test_set = test_set.pivot(index="ISBN", columns="userId", values="rating")
+        dataset = dataset[index_u]
+        test_set = test_set.loc[index_m][index_u]
+
     return dataset, test_set
 
 
 def main():
+    #movie = False
+    movie = True
     ratings = pd.read_csv('ratings.csv')
+    #df_ratings, ratings, ratings_nz, books, users = get_book_data(20)
+    in_min = ratings['rating'].min()
+    in_max = ratings['rating'].max()
 
-    ratings, ratings_v = create_valid(ratings)
+    ratings, ratings_v = create_valid(ratings, movie=movie)
     ratings = ratings.stack().reset_index(name='rating')
     ratings_v = ratings_v.stack().reset_index(name='rating')
 
-    indices = ratings[['userId', 'movieId']].values
+    if movie:
+        ratings.rename(columns={"userId": "user"}, inplace="True")
+        ratings.rename(columns={"movieId": "item"}, inplace="True")
+        ratings_v.rename(columns={"userId": "user"}, inplace="True")
+        ratings_v.rename(columns={"movieId": "item"}, inplace="True")
+    else:
+        ratings.rename(columns={"userId": "user"}, inplace="True")
+        ratings.rename(columns={"ISBN": "item"}, inplace="True")
+        ratings_v.rename(columns={"userId": "user"}, inplace="True")
+        ratings_v.rename(columns={"ISBN": "item"}, inplace="True")
+        ratings['item'] = np.array([i for i in range(ratings.shape[0])])
+
+    indices = ratings[['user', 'item']].values
     values = ratings['rating'].values
-    indices_v = ratings_v[['userId', 'movieId']].values
+    indices_v = ratings_v[['user', 'item']].values
     values_v = ratings_v['rating'].values
 
-    ratings.rename(columns={"userId": "user"}, inplace="True")
-    ratings.rename(columns={"movieId": "item"}, inplace="True")
-    mf = bmf(20, epochs=5, batch_size=1)
-    mf.fit(ratings)
+    bias = Bias(damping=10)
+    mf_g = bmf(20, bias=bias, epochs=1, batch_size=8)
+    mf_g.fit(ratings)
+    mf_m = bmf(30, bias=bias, epochs=1, batch_size=8)
+    mf_m.fit(ratings)
 
-    item_data = pd.DataFrame(mf.item_features_).T
-    user_data = pd.DataFrame(mf.user_features_).T
-    item_data.columns = mf.item_index_
-    user_data.columns = mf.user_index_
+    item_data_g = pd.DataFrame(mf_g.item_features_).T
+    user_data_g = pd.DataFrame(mf_g.user_features_).T
+    item_data_g.columns = mf_g.item_index_
+    user_data_g.columns = mf_g.user_index_
 
-    mask_m = np.in1d(indices_v[:, 1], mf.item_index_)
+    mask_m = np.in1d(indices_v[:, 1], mf_g.item_index_)
     indices_v = indices_v[mask_m]
     values_v = values_v[mask_m]
 
-    user_input = user_data[indices[:, 0]].T
-    item_input = item_data[indices[:, 1]].T
-    user_input_v = user_data[indices_v[:, 0]].T
-    item_input_v = item_data[indices_v[:, 1]].T
+    item_bias_g = (pd.DataFrame(mf_g.bias.item_offsets_).T[indices[:, 1]]).T
+    user_bias_g = (pd.DataFrame(mf_g.bias.user_offsets_).T[indices[:, 0]]).T
+    item_bias_vg = (pd.DataFrame(mf_g.bias.item_offsets_).T[indices_v[:, 1]]).T
+    user_bias_vg = (pd.DataFrame(mf_g.bias.user_offsets_).T[indices_v[:, 0]]).T
 
-    neu = NeuMF([20, 20, 20, 20, 1])  # MLP x 3 , GMF x 1, Out x 1
+    user_input_g = user_data_g[indices[:, 0]].T
+    item_input_g = item_data_g[indices[:, 1]].T
+    user_input_vg = user_data_g[indices_v[:, 0]].T
+    item_input_vg = item_data_g[indices_v[:, 1]].T
+
+    item_data_m = pd.DataFrame(mf_m.item_features_).T
+    user_data_m = pd.DataFrame(mf_m.user_features_).T
+    item_data_m.columns = mf_m.item_index_
+    user_data_m.columns = mf_m.user_index_
+
+    mask_m = np.in1d(indices_v[:, 1], mf_m.item_index_)
+    indices_v = indices_v[mask_m]
+    values_v = values_v[mask_m]
+
+    item_bias_m = pd.DataFrame(mf_m.bias.item_offsets_).T[indices[:, 1]]
+    user_bias_m = pd.DataFrame(mf_m.bias.user_offsets_).T[indices[:, 0]]
+    item_bias_vm = pd.DataFrame(mf_m.bias.item_offsets_).T[indices_v[:, 1]]
+    user_bias_vm = pd.DataFrame(mf_m.bias.user_offsets_).T[indices_v[:, 0]]
+
+    user_input_m = pd.concat([user_data_m[indices[:, 0]], user_bias_m], axis=0).T
+    item_input_m = pd.concat([item_data_m[indices[:, 1]], item_bias_m], axis=0).T
+    user_input_vm = pd.concat([user_data_m[indices_v[:, 0]], user_bias_vm], axis=0).T
+    item_input_vm = pd.concat([item_data_m[indices_v[:, 1]], item_bias_vm], axis=0).T
+
+    neu = NeuMF([80, 40, 20, 20, 1], in_min, in_max)  # MLP x 3 , GMF x 1, Out x 1
     neu.compile(loss='mse', metrics=['mse'])
-    neu.fit([user_input, item_input], values, batch_size=64, epochs=20)
+    with tf.device('/GPU:0'):
+        hist = neu.fit([user_input_g, item_input_g, user_bias_g, item_bias_g, user_input_m, item_input_m], values, batch_size=64, epochs=20)
 
-    neu.evaluate([user_input_v, item_input_v], values_v, batch_size=1)
+    neu.evaluate([user_input_vg, item_input_vg, user_bias_vg, item_bias_vg, user_input_vm, item_input_vm], values_v, batch_size=1)
+
+    y_pred = neu.predict([user_input_vg, item_input_vg, user_bias_vg, item_bias_vg, user_input_vm, item_input_vm], batch_size=1)
+
+    classes = pd.DataFrame(ratings.pivot("item", "user", "rating").mean()).loc[indices_v[:, 0]]
+
+    summary = np.concatenate([y_pred.T, np.array([values_v]), classes.T])
 
     print('testlab')
 
-    #a = neu([user_input, item_input], training=True)
-    #neu.summary()
-    # print(a)
 
-    '''
-    A = tf.SparseTensor(indices=indices, values=values,
-                        dense_shape=[ratings.userId.max() + 1, ratings.movieId.max() + 1])
-    embeddings = 20
-    U = tf.Variable(tf.random.normal([A.shape[0], embeddings]), dtype=tf.float32)
-    V = tf.Variable(tf.random.normal([embeddings, A.shape[1]]), dtype=tf.float32)
-    df_rating = ratings.pivot(index="movieId", columns="userId", values="rating")
-    del_list = []
-    for ind in range(len(indices_v)):
-        if indices_v[ind][0] not in mf.user_index_ or indices_v[ind][1] not in mf.user_index_:
-            print(indices_v[ind])
-            del_list.append(ind)
-    indices_v = np.delete(indices_v, del_list, axis=0)
-
-    optimizer = tf.optimizers.Adam(0.01, 0.8, 0.95)
-
-    trainable_weights = [U, V]
-
-    for step in range(2000):
-        with tf.GradientTape() as tape:
-            A_prime = tf.matmul(U, V)
-            # indexing the result based on the indices of A that contain a value
-            A_prime_sparse = tf.gather(
-                tf.reshape(A_prime, [-1]),
-                indices[:, 0] * tf.shape(A_prime)[1] + indices[:, 1],
-            )
-            loss = tf.reduce_sum(tf.metrics.mean_squared_error(A_prime_sparse, A.values))
-        grads = tape.gradient(loss, trainable_weights)
-        optimizer.apply_gradients(zip(grads, trainable_weights))
-        if step % 20 == 0:
-            print(f"Training loss at step {step}: {loss:.4f}")
-            if loss < 0.8:
-                break                
-    user_input_v = tf.gather(U, indices_v[:, 0], axis=0).numpy()
-    item_input_v = tf.gather(V, indices_v[:, 1], axis=1).numpy().T
-
-    '''
 if __name__ == "__main__":
     main()
--- nochMehrTests.py
+++ nochMehrTests.py
@@ -1,33 +1,287 @@
-import pandas
+import streamlit as st
+# To make things easier later, we're also importing numpy and pandas for
+# working with sample data.
 import numpy as np
+import pandas as pd
 import plotly.graph_objects as go
+from tmdbv3api import TMDb
+from tmdbv3api import Movie
 
-ratings = pandas.read_csv('ratings.csv')
-df_rating = ratings.pivot(index="movieId", columns="userId", values="rating")
 
-# centering
-df_rating = df_rating - df_rating.mean()
-df_rating = df_rating.fillna(0)
-user_std = (df_rating * df_rating).mean() ** 0.5
-# calc cov matrix
-user_corr = df_rating.cov() / (user_std.values.reshape((-1, 1)) @ user_std.values.reshape((1, -1)))
-user_corr = user_corr.fillna(0)
+def main():
+    st.title('Data Science: Recommender Systems')
+    with st.sidebar:
+        st.write('Dataset selection')
 
-sorted_index = pandas.DataFrame(np.argsort(user_corr.values))
-a = sorted_index.iloc[:, [608, 609]]
-b = user_corr.iloc[a.iloc[:, 0].values, a.iloc[:, 1].values]
-largest = []
-for i in range(a.shape[0]):
-    largest.append(user_corr.iloc[a.iloc[i, 0], a.iloc[i, 1]])
-top = a.iloc[(list(np.argsort(largest))[::-1])[0:20]].index
-largest_S = np.sort(largest)[::-1]
-classes = [[] for i_1 in range(20)]
-for i_2 in range(user_corr.shape[0]):
-    sims = user_corr.iloc[[i_2], top].values
-    j = list(np.argsort(sims))[::-1]
-    classes[j[0][0]].append(int(i_2))
-final_index = []
-for cl in classes:
-    final_index += cl
-plot_surf = user_corr.iloc[final_index, final_index]
-print(sorted_index)
+    c_task = st.sidebar.selectbox(
+        "",
+        ("MovieLens", "MovieLens-Tech", "Books")
+    )
+    if c_task == "MovieLens":
+        task1()
+    if c_task == "MovieLens-Tech":
+        task2()
+
+
+def movie_url(ids):
+    tmdb = TMDb()
+    tmdb.api_key = '52f358adec9f89bb2d9a47fceda64fdc'
+    tmdb.language = 'en'
+    tmdb.debug = True
+    movie_api = Movie()
+    urls = []
+    info = []
+    for m_id in ids:
+        m = movie_api.details(int(m_id[0]))
+        urls.append(m.poster_path)
+        info.append(m.title)
+
+    return urls, info
+
+
+def task1():
+    # read movie lens
+    movies = pd.read_csv('movies.csv')
+    ratings = pd.read_csv('ratings.csv')
+    tags = pd.read_csv('tags.csv')
+    links = pd.read_csv('links.csv')
+    st.write('K nearest neighbor centered cosine distance')
+
+    # get settings from sidebar
+    user_number = st.sidebar.selectbox("User ID", (10, 12, 52, 53))
+    k_users = st.sidebar.selectbox("K nearest", (15, 20))
+    list_len = st.sidebar.selectbox("Recommendations", (10, 40))
+    normalization = st.sidebar.selectbox("Normalization", ('centering', 'centering + division by variance'))
+
+    # split the genres per movie
+    movies["genres"] = movies["genres"].str.split('|')
+
+    # rating table
+    df_rating = ratings.pivot(index="movieId", columns="userId", values="rating")
+    df_rating_raw = df_rating
+
+    # normalization procedure
+    if normalization == 'centering + division by variance':
+        df_rating = (df_rating - df_rating.mean()) / df_rating.var() ** 0.5
+    elif normalization == 'centering':
+        df_rating = df_rating - df_rating.mean()
+    df_rating = df_rating.fillna(0)
+    user_std = (df_rating * df_rating).mean() ** 0.5
+
+    # calc cov matrix
+    user_corr = df_rating.cov() / (user_std.values.reshape((-1, 1)) @ user_std.values.reshape((1, -1)))
+    user_corr = user_corr.fillna(0)
+
+    # index of nearest users
+    sorted_index = list(np.argsort(user_corr[user_number]))[::-1]
+
+    # sum of their ratings weighted by the corr
+    corr_k = user_corr.iloc[sorted_index[1:k_users+1]][[user_number]].values
+    ratings_k = df_rating.iloc[:, sorted_index[1:k_users+1]].values
+    w_sum_k = ratings_k @ corr_k
+    mv_rated = df_rating_raw.iloc[:, sorted_index[1:k_users + 1]].notnull().values
+    seen_sim_len = mv_rated @ corr_k
+    seen_sim_len = 1 / (seen_sim_len + (seen_sim_len == 0))
+    recommended = w_sum_k * seen_sim_len
+    rec = recommended.copy()
+
+    # recommended movies
+    unseen = df_rating_raw[user_number].isnull().values
+    recommended = recommended.T[0] * unseen
+    sorted_mov = list(np.argsort(recommended))[::-1]
+    output = movies.iloc[sorted_mov[0:list_len]][['title', 'genres']]
+
+    # percent of best rating
+    color_grade = recommended + abs(rec.min())
+    if rec.max() + abs(rec.min()) > 0:
+        color_grade *= (rec.max() + abs(rec.min())) ** -1
+    else:
+        color_grade *= 1
+    color_grade.sort()
+    color_grade = np.flip(color_grade)
+
+    # display results
+    out2 = recommended[sorted_mov[0:list_len]]
+    rec_header = list(output.columns)
+    rec_header.insert(0, 'predict')
+    colors = []
+    for percentage in color_grade:
+        colors.append('rgba(255,185,15,' + str(percentage ** 2) + ')')
+
+    layout = go.Layout(
+        margin=dict(r=1, l=1, b=20, t=20))
+
+    fig = go.Figure(data=[go.Table(
+        columnwidth=[100, 300, 300],
+        header=dict(values=rec_header,
+                    line_color=['rgb(49, 51, 63)', 'rgb(49, 51, 63)', 'rgb(49, 51, 63)'],
+                    fill_color=['rgb(14, 17, 23)', 'rgb(14, 17, 23)', 'rgb(14, 17, 23)'],
+                    align='center', font=dict(color='white', size=20), height=50
+                    ),
+        cells=dict(values=[np.round(out2, 2), output.title, output.genres],
+                   line_color=['rgb(49, 51, 63)', 'rgb(49, 51, 63)', 'rgb(49, 51, 63)'],
+                   fill_color=[np.array(colors), 'rgb(14, 17, 23)', 'rgb(14, 17, 23)'],
+                   align='center', font=dict(color='white', size=14), height=30
+                   ))
+    ], layout=layout)
+
+    # get movie info / covers
+    url, info = movie_url(links.iloc[sorted_mov[0:3]][['tmdbId']].values)
+
+    st.write('Deine Top auswahl')
+
+    col1, col2, col3 = st.beta_columns(3)
+    col4, col5, col6 = st.beta_columns(3)
+
+    col1.header(info[0])
+    col4.image('https://www.themoviedb.org/t/p/w600_and_h900_bestv2/' + url[0])
+    col2.header(info[1])
+    col5.image('https://www.themoviedb.org/t/p/w600_and_h900_bestv2/' + url[1])
+    col3.header(info[2])
+    col6.image('https://www.themoviedb.org/t/p/w600_and_h900_bestv2/' + url[2])
+
+    st.write('Alle Empfehlungen für dich:')
+    st.write(fig)
+
+
+def task2():
+    # read movie lens
+    movies = pd.read_csv('movies.csv')
+    ratings = pd.read_csv('ratings.csv')
+    tags = pd.read_csv('tags.csv')
+    links = pd.read_csv('links.csv')
+    st.write('K nearest neighbor centered cosine distance')
+
+    # get settings from sidebar
+    k_users = st.sidebar.selectbox("K nearest", (15, 20))
+    normalization = st.sidebar.selectbox("Normalization", ('centering', 'centering + division by variance'))
+
+    # exclude test set
+    ind_exc = np.random.permutation(len(ratings))[0:5000]
+    test_set = ratings.iloc[ind_exc]
+    ratings.drop(index=ind_exc, inplace=True)
+
+    with st.beta_expander("display code"):
+        with st.echo('below'):
+            # split the genres per movie
+            movies["genres"] = movies["genres"].str.split('|')
+
+            # rating table
+            df_rating = ratings.pivot(index="movieId", columns="userId", values="rating")
+            df_test_set = test_set.pivot(index="movieId", columns="userId", values="rating")
+            df_rating_raw = df_rating
+
+            # normalization procedure
+            if normalization == 'centering + division by variance':
+                df_rating = (df_rating - df_rating.mean()) / df_rating.var() ** 0.5
+            elif normalization == 'centering':
+                df_rating = df_rating - df_rating.mean()
+            df_rating = df_rating.fillna(0)
+            user_std = (df_rating * df_rating).mean() ** 0.5
+
+            # calc cov matrix
+            user_corr = df_rating.cov() / (user_std.values.reshape((-1, 1)) @ user_std.values.reshape((1, -1)))
+            user_corr = user_corr.fillna(0)
+
+            # calc errors
+            errors = []
+            num_err = 0
+
+            for user_number, val in df_test_set.iteritems():
+                # index of nearest users
+                sorted_index = list(np.argsort(user_corr[user_number]))[::-1]
+
+                # test set movies
+                test_mov = val.dropna()
+                test_mov_id = list(test_mov.index)
+                train_mov_id = list(df_rating.index)
+                test_mov_id = list(set(test_mov_id) & set(train_mov_id))
+                test_mov = test_mov[test_mov_id]
+                num_err += len(test_mov)
+
+                # sum of their ratings weighted by the corr
+                if len(test_mov) > 0:
+                    corr_k = user_corr.iloc[sorted_index[1:k_users + 1]][[user_number]].values
+                    ratings_k = df_rating.loc[test_mov_id].iloc[:, sorted_index[1:k_users + 1]].values
+                    w_sum_k = ratings_k @ corr_k
+                    mv_rated = df_rating_raw.loc[test_mov_id].iloc[:, sorted_index[1:k_users + 1]].notnull().values
+                    seen_sim_len = mv_rated @ corr_k
+                    seen_sim_len = 1 / (seen_sim_len + (seen_sim_len == 0))
+                    if normalization == 'centering + division by variance':
+                        recommended = w_sum_k * seen_sim_len * df_rating_raw[user_number].var() ** 0.5 + df_rating_raw[user_number].mean()
+                    elif normalization == 'centering':
+                        recommended = w_sum_k * seen_sim_len + df_rating_raw[user_number].mean()
+                    err = np.sum(np.abs(recommended.T - test_mov.values))
+                    errors.append(err)
+
+    st.write("average error of a random test set containing 5000 data points:")
+    error = (1/num_err) * sum(errors)
+    st.write(error)
+
+    sorted_index = pd.DataFrame(np.argsort(user_corr.values))
+    a = sorted_index.iloc[:, [608, 609]]
+    num_cla = 25
+    largest = []
+    for i in range(a.shape[0]):
+        largest.append(user_corr.iloc[a.iloc[i, 0], a.iloc[i, 1]])
+    top = a.iloc[(list(np.argsort(largest))[::-1])[0:num_cla]].index
+
+    classes = [[] for i_1 in range(num_cla)]
+
+    for i_2 in range(user_corr.shape[0]):
+        sims = user_corr.iloc[[i_2], top].values
+        j = list(np.argsort(sims))[::-1]
+        classes[j[0][0]].append(int(i_2))
+    final_index = []
+    for cl in classes:
+        final_index += cl
+    plot_surf = user_corr.iloc[final_index, final_index]
+
+    fig = go.Figure(data=[go.Surface(z=plot_surf)])
+    fig.update_traces(contours_z=dict(show=True, usecolormap=True,
+                                      highlightcolor="limegreen", project_z=True))
+    fig.update_layout(title='Correlation surface', autosize=True,
+                      width=800, height=700,
+                      margin=dict(l=1, r=1, b=40, t=40)
+                      )
+
+    if False:
+        # surface plot
+        sorted_index = pd.DataFrame(np.argsort(user_corr.values))
+        a = sorted_index.iloc[:, [608, 609]]
+        num_cla = 25
+        largest = []
+        for i in range(a.shape[0]):
+            largest.append(user_corr.iloc[a.iloc[i, 0], a.iloc[i, 1]])
+        top = a.iloc[(list(np.argsort(largest))[::-1])[0:num_cla]].index
+
+        classes = [[] for i_1 in range(num_cla)]
+
+        for i_2 in range(user_corr.shape[0]):
+            sims = user_corr.iloc[[i_2], top].values
+            j = list(np.argsort(sims))[::-1]
+            classes[j[0][0]].append(int(i_2))
+        final_index = []
+        for cl in classes:
+            final_index += cl
+        plot_surf = user_corr.iloc[final_index, final_index]
+
+        fig = go.Figure(data=[go.Surface(z=plot_surf)])
+        fig.update_traces(contours_z=dict(show=True, usecolormap=True,
+                                          highlightcolor="limegreen", project_z=True))
+        fig.update_layout(title='Correlation surface', autosize=True,
+                          width=800, height=700,
+                          margin=dict(l=1, r=1, b=40, t=40)
+                          )
+
+        st.write(fig)
+
+    st.write(fig)
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
