9cf48be63f92ee07c40491adc24a2f3e484f5604
Lucas <lucas.j.menger@gmail.com>
2021-06-22 17:18:35 +0200

    ai table

--- Streamlit_knn.py
+++ Streamlit_knn.py
@@ -870,7 +870,7 @@ def task6():
     df_rating, ratings, df_rating_nonzero, books, users = get_book_data(200)
 
     # get settings from sidebar
-    user_number = st.sidebar.selectbox("User ID", (6, 19, 24727, 79186, 145434, 195550, 207782))
+    user_number = st.sidebar.selectbox("User ID", (254, 4017, 24921, 79186, 145449, 195694, 222050))
     k_users = st.sidebar.selectbox("K nearest", (5, 15, 20))
     list_len = st.sidebar.selectbox("Recommendations", (10, 40))
 
